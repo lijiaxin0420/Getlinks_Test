4e9f3a3ce244ac2ded8e9f18dd7de77f3a99b7af
lijiaxin0420 <32337570+lijiaxin0420@users.noreply.github.com>
2017-10-01 04:45:47 +0800

    Update feature.R

--- feature.R
+++ feature.R
@@ -28,7 +28,7 @@ hist1  = simplify2array(sapply(full_a[,'paymenthistory1'], pay_text))
 hist2  = simplify2array(sapply(full_a[,'paymenthistory2'], pay_text))
 
 # payment_history_avg_dpd_0_29_bucket
-hist1_29 = sapply(hist1, function(x)  sum(x<30, na.rm = TRUE)) # Count of accounts that is in 0-29 days past due bucket throughout the payment history.
+hist1_29 = sapply(hist1, function(x)  sum(x<30, na.rm = TRUE)) # Count of accounts that is in 0-29 days past due bucket throughout the payment history, , with less than 30 days past due being a ‘punctual’ payment and therefore lower risk.
 hist2_29 = sapply(hist2, function(x)  sum(x<30, na.rm = TRUE))
 x_a$pay_dpd_0_29_bucket = apply(cbind(hist1_29, hist2_29), 1, sum)
 
@@ -44,18 +44,21 @@ hist2_total = sapply(hist2, function(x)  length(x))
 x_a$payment_history_mean_length = apply(cbind(hist1_total, hist2_total), 1, sum)
 
 # pay_dpd_ratio
+# Ratio of the average number of payments falling into the 30-or-more-days-past-due bag against average number of payments. Indicating duration of non-payment throughout payment history.                     
 x_a$pay_dpd_ratio = ifelse(x_a[,payment_history_mean_length] == 0, NA, x_a[,pay_dpd_0_29_bucket]/x_a[,payment_history_mean_length])
-
+                     
 # Average and merge
 x_a = x_a[, lapply(.SD,mean), by = 'customer_no', .SDcols = names(x_a)[3:ncol(x_a)]]
 
-# total_diff_lastpaymt_opened_dt
+# total_diff_lastpaymt_opened_dt 
+# The total duration between last payment date and account opened date of all accounts. Shows recency of payment.                     
 full_a$total_diff_lastpaymt_opened_dt = as.numeric(full_a[,'last_paymt_dt'] - full_a[,'opened_dt'])
 temp = data.table(full_a[,c('customer_no', 'total_diff_lastpaymt_opened_dt')])
-temp = temp[, lapply(.SD, sum), by = 'customer_no', .SDcols = c('total_diff_lastpaymt_opened_dt')]
+temp = temp[, lapply(.SD, sum), by = 'customer_no', .SDcols = c('total_diff_lastpaymt_opened_dt')]# Apply a function on a subset of columns.
 x_a  = merge(x_a, temp, 'customer_no')
 
 # min_months_last_30_plus
+# Minimum number of months that passed before first 30-or-more-days-past-due bag appeared, showing punctuality                     
 months_dpd_30_1 = mapply(function(x,y){if (y>0){return(x)} else{return(NA)}}, hist1_29, hist1_30)
 months_dpd_30_2 = mapply(function(x,y){if (y>0){return(x)} else{return(NA)}}, hist2_29, hist2_30)
 temp = data.frame('customer_no' = full_a$customer_no)
@@ -65,6 +68,7 @@ temp = temp[, lapply(.SD, min), by = 'customer_no', .SDcols = c('min_months_last
 x_a  = merge(x_a, temp)
 
 # utilisation_trend
+# [total_cur_balance_amt / total_credit_limit] / [mean_cur_balance_amt / (mean_credit_limit+ mean_cashlimit)]                     
 temp = full_a[,c('customer_no','cur_balance_amt','creditlimit','cashlimit')]
 temp$utilisation_trend = ((ave(temp$cur_balance_amt, temp$customer_no, FUN = sum)/
                              ave(temp$creditlimit, temp$customer_no, FUN = sum))/
@@ -76,6 +80,7 @@ temp = data.table(unique(temp[,c(1,ncol(temp))]), key = 'customer_no')
 x_a  = merge(x_a, temp)
 
 # ratio_currbalance_creditlimit
+# Ratio of current balance to credit limit, showing how much applicant is dependent on credit.
 temp = full_a[,c('customer_no','cur_balance_amt','creditlimit')]
 temp$ratio_currbalance_creditlimit = ave(full_a$cur_balance_amt, full_a$customer_no, FUN = sum) / 
   ave(full_a$creditlimit, full_a$customer_no, FUN = sum)
@@ -84,6 +89,7 @@ temp = data.table(unique(temp[,c(1,ncol(temp))]), key = 'customer_no')
 x_a  = merge(x_a, temp)
 
 # count_enquiry_recency_365
+# Number of enquiries made in the last 365 days. Shows frequency of applications.                     
 temp   = full_e[, lapply(.SD, max), by = 'customer_no', .SDcols = 'enquiry_dt']
 temp   = rename(temp, c('enquiry_dt' = 'max_dt'))
 full_e = merge(full_e, temp)
@@ -95,6 +101,7 @@ x_e  = merge(x_e, temp)
 x_e  = x_e[,c(1,3)]
 
 # count_enquiry_recency_90
+# Number of enquiries made in the last 90 days. Shows frequency of applications.                     
 full_e$recent_flag = as.numeric((full_e$max_dt - full_e$enquiry_dt) < 91)
 temp = full_e[,c('customer_no','recent_flag')]
 temp = temp[, lapply(.SD, sum), by = 'customer_no', .SDcols = 'recent_flag']
@@ -102,11 +109,13 @@ temp = rename(temp, c('recent_flag' = 'count_enquiry_recency_90'))
 x_e  = merge(x_e, temp)
 
 # mean_diff_open_enquiry_dt
+# Average difference between enquiry dt_opened date and enquiry date. Shows recency of the previous applications.
 full_e$diff_oe = as.numeric(full_e$dt_open - full_e$enquiry_dt)
 x_e = merge(x_e, full_e[, lapply(.SD, mean), by = 'customer_no', .SDcols = 'diff_oe'])
 x_e = rename(x_e, c('diff_oe' = 'mean_diff_open_enquiry_dt'))
 
 # max_freq_enquiry
+# Most frequent enquiry purpose.                     
 Mode = function(x) {
   y = unique(x)
   y[which.max(tabulate(match(x, y)))]
